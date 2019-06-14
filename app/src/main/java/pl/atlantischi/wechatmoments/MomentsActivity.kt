4218d7a3e267c63704103a703c6b36633ecb8b40
luoxuan <xuanl1@jumei.com>
2019-06-14 12:28:44 +0800

    Initial commit

---
+++ app/src/main/java/pl/atlantischi/wechatmoments/MomentsActivity.kt
@@ -0,0 +1,13 @@
+package pl.atlantischi.wechatmoments
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MomentsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moments)
+    }
+
+}
